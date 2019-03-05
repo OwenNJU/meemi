be689e953ec2fbd11f0670e2c52c412237aa42fe
Wenhao Zhu <zhuwh@nlp.nju.edu.cn>
2019-03-05 15:45:31 +0800

    add some comments in get_means.py

--- utils/get_means.py
+++ utils/get_means.py
@@ -2,12 +2,17 @@ import sys, gensim
 import numpy as np
 import re
 
+# load pretrained word vector
 models = [gensim.models.KeyedVectors.load_word2vec_format(arg, binary=arg.endswith(".bin")) for arg in sys.argv[1:]]
+# str.endswith(suffix) : detect whether str end with suffix, if yes return ture
+# sys.argv[1:]:there maybe several word2vec pretrained texts
 
 def default_get(x, d):
     try:
         return d[x]
+        #for example : model['love'] = wordvec of 'love'
     except KeyError:
+        #KeyError : x doen't exist in the dict d
         return []
 
 def get_mean(words, models):
@@ -16,15 +21,21 @@ def get_mean(words, models):
 
 def zip_rep(words, vector):
     rep_vector = [vector for i in range(0, len(words))]
+    #repeat vector
     return zip(words, rep_vector)
 
 for i, line in enumerate(sys.stdin):
+    #sys.stdin : input dictionary
     words = line.strip().split(" ")
+    #line.strip() : delete " " at both sides of line
     if len(words) != len(models):
         sys.stderr.write("Number of models and words not matching at line " + str(i) + "! " + str(len(words)) + " " + str(len(models)) + "\n")
         continue
     s = [" ".join([word] + [str(x) for x in vector]) for word, vector in zip_rep(words, get_mean(words, models))]
+    # s = ['word wordvector', 'word wordvector']
     lengths = [x.strip().split() for x in s]
     lengths_condition = [len(x) > 3 for x in lengths]
+    # if source and target word both have counterpart wordvector
     if all(lengths_condition):
         print("\t".join(s))
+    #output the line word \t wordvector \t word \t wordvector
